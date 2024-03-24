e01f2bd513b80cdeb18308f5e0b23b46218adc79
Gustavo Torres <gtn.280693@gmail.com>
2024-03-23 23:45:08 -0400

    modal cuenta atras agregado en pantalla de inicio

---
+++ js/modal.js
@@ -0,0 +1,21 @@
+const second = 1000,
+      minute = second * 60,
+      hour = minute * 60,
+      day = hour * 24;
+
+let countDown = new Date('Jul 02, 2025 16:38:00').getTime(),
+    x = setInterval(function() {
+
+      let now = new Date().getTime(),
+          distance = countDown - now;
+
+      document.getElementById('dias').innerText = Math.floor(distance / (day)),
+        document.getElementById('horas').innerText = Math.floor((distance % (day)) / (hour)),
+        document.getElementById('minutos').innerText = Math.floor((distance % (hour)) / (minute)),
+        document.getElementById('segundos').innerText = Math.floor((distance % (minute)) / second);
+
+    }, second)
+
+    $( document ).ready(function() {
+        $('#exampleModalCenter').modal('toggle')
+    });
